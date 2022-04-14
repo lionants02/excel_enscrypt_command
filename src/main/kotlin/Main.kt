ce791024a1a7bf2d028ae13296756add40262f34
Max Thanachai Thongkum <singmax161@gmail.com>
2022-04-14 15:32:03 +0700

    remove debug message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -29,11 +29,7 @@ import java.io.File
 
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // println("Program arguments: ${args.joinToString()}")
     val enc: EncryptExcel = EncryptXlsx()
     val file = File(args[0])
     require(file.isFile) { "Cannot file" }
